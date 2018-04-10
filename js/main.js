66771dae4895b6eee8f444ea24c29556a6c50e58
Marco Espinosa <marco@MacBook-Pro-de-Dennis-2.local>
2018-04-10 16:03:01 -0500

    primera version

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*$('#carouselExampleSlidesOnly').carousel();
+$('#carouselExampleControls').carousel({
+    pause: true
+});
+$('#carouselExampleControls1').carousel();
+$('#carouselExampleControls2').carousel();
+$('#carouselExampleControls3').carousel();*/
